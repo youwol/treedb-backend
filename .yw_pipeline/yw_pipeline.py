232e2eb092fc97a131f2d9464e8a005aca3e94c5
youwol-guillaume <greinisch@youwol.com>
2022-09-29 10:38:16 +0200

    :wrench: simplify yw_pipeline.py thanks to factorized code

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,8 +1,6 @@
 from youwol.environment.models import IPipelineFactory
 from youwol.environment.youwol_environment import YouwolEnvironment
-from youwol.pipelines.docker_k8s_helm import InstallHelmStepConfig, get_helm_app_version
-from youwol.pipelines.pipeline_fastapi_youwol_backend import pipeline, PipelineConfig, DocStepConfig, \
-    CustomPublishDockerStepConfig
+from youwol.pipelines.pipeline_fastapi_youwol_backend import get_backend_apps_yw_pipeline
 from youwol_utils.context import Context
 
 
@@ -12,28 +10,4 @@ class PipelineFactory(IPipelineFactory):
         super().__init__(**kwargs)
 
     async def get(self, env: YouwolEnvironment, context: Context):
-        docker_repo = env.k8sInstance.docker.get_repo("gitlab-docker-repo")
-
-        async with context.start(
-                action="Pipeline creation for treedb-backend",
-                with_attributes={'project': 'treedb-backend'}
-        ) as ctx:  # type: Context
-
-            config = PipelineConfig(
-                tags=["treedb-backend"],
-                k8sInstance=env.k8sInstance,
-                dockerConfig=CustomPublishDockerStepConfig(
-                    dockerRepo=docker_repo,
-                    imageVersion=lambda project, _ctx: get_helm_app_version(project.path)
-                ),
-                docConfig=DocStepConfig(),
-                helmConfig=InstallHelmStepConfig(
-                    namespace="apps",
-                    chartPath=lambda project, _ctx: project.path / 'chart',
-                    valuesPath=lambda project, _ctx: project.path / 'chart' / 'values.yaml'
-                )
-            )
-            await ctx.info(text='Pipeline config', data=config)
-            result = await pipeline(config, ctx)
-            await ctx.info(text='Pipeline', data=result)
-            return result
+        return await get_backend_apps_yw_pipeline(name="treedb-backend", context=context)
